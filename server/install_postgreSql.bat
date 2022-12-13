bfa71642475806a1ff02acf192db3986be2134b5
Uvays Bakoyev <norches74@gmail.com>
2022-12-13 19:33:07 +0500

    first commit

---
+++ server/install_postgreSql.bat
@@ -0,0 +1,4 @@
+echo off
+.\postgresql-14.6-1-windows-x64 --mode unattended --unattendedmodeui minimal --superpassword masterkey
+set PGPASSWORD=masterkey
+echo SELECT 'CREATE DATABASE chayxanshik' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'chayxanshik')\gexec |"%ProgramFiles%\PostgreSQL\14\bin\psql.exe" -U postgres
