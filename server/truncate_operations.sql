f2d8d47e197fa0d919a280170bfc24ecd8b2f6c4
Uvays Bakoyev <norches74@gmail.com>
2023-03-14 00:47:30 +0500

    UI v0.27.1

--- server/truncate_operations.sql
+++ server/truncate_operations.sql
@@ -1,11 +1,20 @@
 SET session_replication_role = replica;
 
+/* Чеки */
 TRUNCATE TABLE receipt_orderline CASCADE;
 TRUNCATE TABLE receipt_receiptpayment CASCADE;
 TRUNCATE TABLE receipt_order CASCADE;
 TRUNCATE TABLE receipt_receipt CASCADE;
+
+/* Склад */
 TRUNCATE TABLE warehouse_purchase CASCADE;
 TRUNCATE TABLE warehouse_purchaseline CASCADE;
+TRUNCATE TABLE warehouse_return CASCADE;
+TRUNCATE TABLE warehouse_returnline CASCADE;
+TRUNCATE TABLE warehouse_entry CASCADE;
+TRUNCATE TABLE warehouse_entryline CASCADE;
+TRUNCATE TABLE warehouse_writeoff CASCADE;
+TRUNCATE TABLE warehouse_writeoffline CASCADE;
 TRUNCATE TABLE warehouse_consumption CASCADE;
 
 SET session_replication_role = DEFAULT;
