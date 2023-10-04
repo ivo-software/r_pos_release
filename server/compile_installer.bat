ec7615c58a6856069585bb6cafa0dbba0a7d59c5
Uvays Bakoyev <n0rches@ya.ru>
2023-10-04 18:18:53 +0500

    API v1.17.1 waiter_app connection bug fix

--- server/compile_installer.bat
+++ server/compile_installer.bat
@@ -1,3 +1,3 @@
-set LATEST_APP_VERSION=1.17.0
+set LATEST_APP_VERSION=1.17.1
 "C:\Program Files (x86)\NSIS\makensis.exe" /DOUTPUT_NAME="installer/r_pos_api_setup_${LATEST_VERSION}.exe" /DLATEST_VERSION=%LATEST_APP_VERSION% /DINCLUDE_SQL=0 installer.nsi 
 "C:\Program Files (x86)\NSIS\makensis.exe" /DOUTPUT_NAME="installer/r_pos_api_setup_${LATEST_VERSION}_with_PgSQL.exe" /DLATEST_VERSION=%LATEST_APP_VERSION% /DINCLUDE_SQL=1 installer.nsi 
