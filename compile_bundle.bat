0648a0ff73e984efee57e09256850f10b1dc9466
Uvays <n0rches@ya.ru>
2024-06-14 04:40:45 +0500

    UI v1.32.1

--- compile_bundle.bat
+++ compile_bundle.bat
@@ -1,6 +1,6 @@
 @echo off
 set API_VERSION=1.32.0
-set UI_VERSION=1.32.0
+set UI_VERSION=1.32.1
 set WAITER_APP_VERSION=1.12.0(26)
 set Q_MONITOR_APP_VERSION=1.1.1(35)
 "C:\Program Files (x86)\NSIS\makensis.exe" /DOUTPUT_NAME="installer/r_pos_api_update_${LATEST_VERSION}.exe" /DLATEST_VERSION=%API_VERSION% /DINCLUDE_SQL=0 server\installer.nsi 
