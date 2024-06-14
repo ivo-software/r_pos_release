b052ac8ffa0007470cd4bd912e89fe924bee0f0a
Uvays <n0rches@ya.ru>
2024-06-14 16:18:44 +0500

    Waiter v1.13.0

--- compile_bundle.bat
+++ compile_bundle.bat
@@ -1,7 +1,7 @@
 @echo off
 set API_VERSION=1.32.0
 set UI_VERSION=1.32.1
-set WAITER_APP_VERSION=1.12.0(26)
+set WAITER_APP_VERSION=1.13.0(27)
 set Q_MONITOR_APP_VERSION=1.1.1(35)
 "C:\Program Files (x86)\NSIS\makensis.exe" /DOUTPUT_NAME="installer/r_pos_api_update_${LATEST_VERSION}.exe" /DLATEST_VERSION=%API_VERSION% /DINCLUDE_SQL=0 server\installer.nsi 
 "C:\Program Files (x86)\NSIS\makensis.exe" /DOUTPUT_NAME="installer/r_pos_api_${LATEST_VERSION}_PgSQL_server.exe" /DLATEST_VERSION=%API_VERSION% /DINCLUDE_SQL=1 server\installer.nsi 
